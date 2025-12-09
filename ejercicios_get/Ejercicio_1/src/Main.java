5b933d495c4fd8a24fd0587ecf65e58ebd35c89b
ekai2 <ekaitz.maestu.48854@ikasle.egibide.org>
2025-12-09 09:43:36 +0100

    Version 1.1

--- ejercicios_get/Ejercicio_1/src/Main.java
+++ ejercicios_get/Ejercicio_1/src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static Scanner teclado = new Scanner(System.in);
+    private static String [] jugadores = {"Jon", "Urtzi", "Ekaitz Maestu", "Ekaitz Ruiz", "Aritz", "Josemi"};
+    private static String [] roles = {"PORTERO", "DEFENSA", "CENTROCAMPISTA", "DELANTERO"};
+
+
+    public static void main(String[] args) {
+     Set <String> Jugadoresdisponibles = new HashSet<>();
+
+        for (String jugador : jugadores) {
+            Jugadoresdisponibles.add(jugador);
+        }
+
+        for (String rol : roles) {
+            System.out.println("Seleccione un jugador para el rol de " + rol + ":");
+            for (String jugador : Jugadoresdisponibles) {
+                System.out.println("- " + jugador);
+            }
+            String seleccionado = null;
+            while (!Jugadoresdisponibles.contains(seleccionado)) {
+                seleccionado = teclado.nextLine();
+                if (Jugadoresdisponibles.contains(seleccionado)) {
+                    Jugadoresdisponibles.remove(seleccionado);
+                    System.out.println(seleccionado + " ha sido asignado como " + rol + ".");
+                    break;
+                } else {
+                    System.out.println("Jugador no disponible. Por favor, seleccione otro jugador:");
+                }
+            }
+
+        }
         }
     }
-}
