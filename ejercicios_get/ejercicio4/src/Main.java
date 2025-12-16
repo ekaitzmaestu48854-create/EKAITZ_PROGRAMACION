7b6f6ab086a6c4babc875254de8773ae5bc337ad
ekai2 <ekaitz.maestu.48854@ikasle.egibide.org>
2025-12-16 09:47:51 +0100

    Ejercicio 4 terminado

--- ejercicios_get/ejercicio4/src/Main.java
+++ ejercicios_get/ejercicio4/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.LinkedHashMap;
 import java.util.Scanner;
+import java.lang.String;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -13,37 +14,99 @@ public class Main {
     public static void AnadirNumero(){
         String nombre;
         int numero;
-        boolean continuar = false;
-        sc.nextLine();
-        do {
+        String respuesta;
+
+
             System.out.println("Introduce el nombre: ");
             nombre = sc.nextLine();
             System.out.println("Introduce el numero: ");
             numero = sc.nextInt();
+            sc.nextLine();
+
 
             telefono.put(nombre, numero);
 
-            System.out.println("¿Quieres añadir otro numero? (s/n)");
-            continuar = sc.nextBoolean();
-            sc.nextLine(); // Limpiar el buffer
 
-        }while (continuar == true);
+            System.out.println("¿Quieres añadir otro numero? (s/n)");
+            respuesta = sc.nextLine();
+
+
+            if (respuesta.equalsIgnoreCase("s")) {
+                AnadirNumero();
+            } else {
+                Menu();
+            }
+
     }
 
-    public static void BuscarNumero(){
-        int numeroBuscado;
-        System.out.println("Introduce el numero a buscar: ");
-        numeroBuscado = sc.nextInt();
 
-        for (String nombre : telefono.keySet()) {
-            if (telefono.isEmpty()) {
-                System.out.println("La agenda esta vacia");
-            } else if (telefono.equals(numeroBuscado)) {
-                System.out.println("El numero " + numeroBuscado + " pertenece a " + nombre);
+    public static void BorrarNumero(){
+        if (telefono.isEmpty()) {
+            System.out.println("La agenda está vacía.");
+            return;
+        }
+        System.out.print("¿Qué número desea borrar?: ");
+        int numeroBorrar = Integer.parseInt(sc.nextLine());
+        for (String key : telefono.keySet()) {
+            if (telefono.get(key) == numeroBorrar) {
+                telefono.remove(key);
+                System.out.println("Número borrado: " + key + " - " + numeroBorrar);
             }
         }
+        Menu();
+    }
+
+// El Fallo que tenia era que estaba trabajando con Integers y no con int, por lo que al comparar con == no funcionaba
+    public static void BuscarNumero() {
+        if (telefono.isEmpty()) {
+            System.out.println("La agenda está vacía.");
+            return;
+        }
+        System.out.print("¿Qué número desea buscar?: ");
+        int numeroBuscar = Integer.parseInt(sc.nextLine());
+        for (String key : telefono.keySet()) {
+            if (telefono.get(key) == numeroBuscar) {
+                System.out.println("Número encontrado: " + key + " - " + telefono.get(key));
+                return;
+            }
+        }
+        Menu();
+    }
 
 
+    public static void BorrarporNombre(){
+        if (telefono.isEmpty()) {
+            System.out.println("La agenda esta vacia");
+            return;
+        }
+        System.out.println("Introduce el nombre que quieres borrar: ");
+        String nom = sc.nextLine();
+
+        if (telefono.containsKey(nom)) {
+            telefono.remove(nom);
+            System.out.println("El contacto " + nom + " ha sido borrado.");
+        } else {
+            System.out.println("El contacto " + nom + " no existe en la agenda.");
+        }
+
+    Menu();
+    }
+
+
+    public static void BuscarPorNombre(){
+        if (telefono.isEmpty()) {
+            System.out.println("La agenda esta vacia");
+            return;
+        }
+        System.out.println("Introduce el nombre que quieres buscar: ");
+        String nom = sc.nextLine();
+
+        if (telefono.containsKey(nom)) {
+            System.out.println("El numero de " + nom + " es: " + telefono.get(nom));
+        } else {
+            System.out.println("El contacto " + nom + " no existe en la agenda.");
+        }
+    Menu();
     }
 
     public static void Menu(){
@@ -59,21 +122,22 @@ public class Main {
                 "6. Salir");
         System.out.println("Elige una opcion: ");
         opcion = sc.nextInt();
+        sc.nextLine();
 
         switch (opcion){
             case 1: AnadirNumero();
 
                 break;
-            case 2:
+            case 2: BorrarNumero();
 
                 break;
             case 3: BuscarNumero();
 
                 break;
-            case 4:
+            case 4: BorrarporNombre();
 
                 break;
-            case 5:
+            case 5: BuscarPorNombre();
 
                 break;
             case 6:
